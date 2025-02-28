ad937c5f945ca1fbc409f32c5923e718542b0a8a
Nicolas <nicolastulio@gmail.com>
2025-02-28 19:16:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
           {gameOver(number) ? (
             <img
               className={`img-win ${gameOver(number) ? "img-win--show" : ""}`}
-              src="../public/you win.gif"
+              src="../public/you_win.gif"
             />
           ) : null}
         </p>
